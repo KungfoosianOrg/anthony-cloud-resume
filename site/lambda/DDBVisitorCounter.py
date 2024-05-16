72de6fd7b133cf6613fc0e4f6284bd2f490e8c6a
Anthony <anthonyvovo123@gmail.com>
2024-05-16 13:08:24 -0700

    new branch

---
+++ site/lambda/DDBVisitorCounter.py
@@ -0,0 +1,59 @@
+class DDBVisitorCounter:
+    def __init__(self, DDBResource):
+        self.client = DDBResource['client']
+        self.table_name = DDBResource['table_name']
+        self.counter_table_entry = DDBResource['counter_table_entry']
+        self.counter = self.get_counter_entry() 
+
+    
+    def get_counter_entry(self):
+        """
+            Gets visitor counter
+
+            Tests: get entry that exist, get entry that doesn't
+        """
+        try:
+            response = self.client.get_item( 
+                                    TableName = self.table_name,
+                                    Key = self.counter_table_entry
+                                    )
+            
+            if 'Item' not in response.keys():
+                raise Exception('Table entry does not exist')
+            
+            return int(response['Item']['timesVisited']['N'])
+
+        except Exception as e:
+            raise e
+        
+
+    def increase_counter(self):
+        """
+            Increase counter by 1
+        """
+        self.counter += 1
+
+
+    def reset_counter(self):
+        """
+            Reset counter to 1
+        """
+        self.counter = 1
+
+
+    def update_ddb(self):
+        """
+            Updates DDB entry visitorCounter to whichever number self.counter has
+            @return int updated counter number
+        """
+        try:
+            self.client.update_item(
+                         TableName = self.table_name,
+                         Key = self.counter_table_entry,
+                         ExpressionAttributeValues={ ':newValue': { 'N': str(self.counter) } },
+                         UpdateExpression = 'SET timesVisited = :newValue',
+                         ReturnValues = 'UPDATED_NEW'
+                        )
+            
+        except Exception as e:
+            raise e
---
+++ site/lambda/lambda_function.py
@@ -0,0 +1,45 @@
+from boto3 import client
+from json import dumps
+from os import environ
+from LambdaResponse import LambdaResponse
+from DDBVisitorCounter import DDBVisitorCounter
+
+_RESPONSE_DEFAULT = {
+    'statusCode': 200,
+    'body': 'request processed'
+}
+
+_VISITORCOUNTER_RESOURCE = {
+    'client': client('dynamodb'),
+    'table_name': environ.get('DDB_TABLE_ARN'),
+    'counter_table_entry': {
+        'id': { 'S': '0' }
+    }
+}
+
+
+
+def lambda_handler(event, context):
+    # ignore requests that are not POST to /visitor-count
+    if event['resource'] != '/visitor-count' or event['httpMethod'] != 'POST':
+        return LambdaResponse(response=_RESPONSE_DEFAULT).json
+    
+    try:
+        myVisitorCounter = DDBVisitorCounter(DDBResource=_VISITORCOUNTER_RESOURCE)
+
+        myVisitorCounter.increase_counter()
+
+        if myVisitorCounter.counter > 100:
+            myVisitorCounter.reset_counter()
+            
+
+        myVisitorCounter.update_ddb()
+
+        return LambdaResponse(response={
+                                        'statusCode': 200,
+                                        'body': dumps({ 'timesVisited': str(myVisitorCounter.counter) })
+                                        }).json
+    except Exception as e:
+        print(f'Encountered an exception: {e}')
+
+    return
