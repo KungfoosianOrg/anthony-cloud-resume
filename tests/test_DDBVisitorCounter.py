ac48730288e1cc8d643a0f8844966ae1d082cd21
Anthony <anthonyvovo123@gmail.com>
2024-05-17 14:48:57 -0700

    added setup and teardown

--- tests/test_DDBVisitorCounter.py
+++ tests/test_DDBVisitorCounter.py
@@ -8,7 +8,46 @@ import sys
 from back_end import DDBVisitorCounter
 
 
-@mock_aws
+# @mock_aws
 class TestDDBVisitorCounter(TestCase):
     def setUp(self):
-        DDBVisitorCounter()
+        self.mock_aws = mock_aws()
+        self.mock_aws.start()
+
+        _MOCK_TABLE_NAME = 'test-ddb-table'
+
+        # to test the code, need to create a DDB table with an entry of id of 0, type string (S), and a timesVisited entry type number (N)
+        dynamodb = client('dynamodb')
+
+        # creating the table
+        table = dynamodb.create_table(
+            AttributeDefinitions = [
+                {
+                    'AttributeName': 'id',
+                    'AttributeType': 'S'
+                }
+            ],
+            TableName = _MOCK_TABLE_NAME,
+            KeySchema = [
+                {
+                    'AttributeName': 'id',
+                    'KeyType': 'HASH'
+                }
+            ]
+        )
+
+        # add entry of timesVisited (type N) to entry of id 0
+        counter_entry = dynamodb.put_item(
+            TableName = _MOCK_TABLE_NAME,
+            Item = {
+                'id': {
+                    'S': '0'
+                },
+                'timesVisited': {
+                    'N': '0'
+                }
+            }
+        )
+
+    def tearDown(self) -> None:
+        self.mock_aws.stop()
