44d84783f4a0856feb682c3ff8115a472e74abcf
Anthony <anthonyvovo123@gmail.com>
2024-05-17 15:47:29 -0700

    working on test

--- tests/test_DDBVisitorCounter.py
+++ tests/test_DDBVisitorCounter.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from boto3 import client
 from moto import mock_aws
-import os
+from os import environ
 import json
 import sys
 
@@ -14,20 +14,22 @@ class TestDDBVisitorCounter(TestCase):
         self.mock_aws = mock_aws()
         self.mock_aws.start()
 
-        _MOCK_TABLE_NAME = 'test-ddb-table'
+        self.mocked_ddb_table_name = 'test-ddb-table'
+        environ['DDB_TABLE_ARN'] = self.mocked_ddb_table_name
 
         # to test the code, need to create a DDB table with an entry of id of 0, type string (S), and a timesVisited entry type number (N)
-        dynamodb = client('dynamodb')
+        dynamodb = client('dynamodb', region_name='us-west-1')
 
         # creating the table
-        table = dynamodb.create_table(
+        dynamodb.create_table(
             AttributeDefinitions = [
                 {
                     'AttributeName': 'id',
                     'AttributeType': 'S'
                 }
             ],
-            TableName = _MOCK_TABLE_NAME,
+            TableName = self.mocked_ddb_table_name,
+            BillingMode = 'PAY_PER_REQUEST',
             KeySchema = [
                 {
                     'AttributeName': 'id',
@@ -37,8 +39,8 @@ class TestDDBVisitorCounter(TestCase):
         )
 
         # add entry of timesVisited (type N) to entry of id 0
-        counter_entry = dynamodb.put_item(
-            TableName = _MOCK_TABLE_NAME,
+        dynamodb.put_item(
+            TableName = self.mocked_ddb_table_name,
             Item = {
                 'id': {
                     'S': '0'
@@ -49,5 +51,26 @@ class TestDDBVisitorCounter(TestCase):
             }
         )
 
+        self.mocked_visitorcounter_resource = {
+            'client': client('dynamodb', region_name='us-west-1'),
+            'table_name': self.mocked_ddb_table_name,
+            'counter_table_entry': {
+                'id': { 'S': '0' }
+            }
+        }
+
+        # try:
+        #     self.mocked_ddbvisitorcounter_class = DDBVisitorCounter(DDBResource=mocked_visitorcounter_resource)
+        # except Exception as e:
+        #     print(f'Ran into exception: {e}')
+
     def tearDown(self) -> None:
         self.mock_aws.stop()
+
+    def test_get_counter_entry(self):
+        mocked_ddbvisitorcounter_class = DDBVisitorCounter(DDBResource=self.mocked_visitorcounter_resource)
+        test_return_value = mocked_ddbvisitorcounter_class.get_counter_entry()
+        # print(self.mocked_ddbvisitorcounter_class)
+
+        self.assertTrue(test_return_value.isnumeric())
+
