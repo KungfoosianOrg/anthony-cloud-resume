2b047d5a384bb19d3afb5e39dc53d512ba0677d1
Anthony Vo <anthonyvovo123@gmail.com>
2024-07-12 23:28:22 -0700

    getting serious about unit testing

---
+++ tests/test_sendSlackMessage.py
@@ -0,0 +1,15 @@
+import moto
+import unittest
+from aws.sendSlackMessage.lambda_function import lambda_handler
+
+import json
+import boto3
+import urllib3
+import os
+
+
+
+
+
+class TestSendSlackMessage(unittest.TestCase):
+    @unittest.mock('lambda_handler.ssm')
