3649ed724408780317a82c25f8a44437418ebef1
Anthony Vo <anthonyvovo123@gmail.com>
2024-10-28 13:05:01 -0700

    Update main.tf

--- terraform/modules/sam-s3-cloudfront-static-site-hsts/main.tf
+++ terraform/modules/sam-s3-cloudfront-static-site-hsts/main.tf
@@ -246,6 +246,7 @@ data "aws_iam_policy_document" "cicd-permissions" {
       "s3:PutObject",
       "s3:PutObjectAcl",
       "s3:ListBucket",
+      "s3:ListBucketVersions",
       "s3:DeleteObject",
       "s3:GetAccelerateConfiguration",
       "s3:GetObject",
